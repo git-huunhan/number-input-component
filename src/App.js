96536c95af7579889410e118354bd7e39e59be87
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-15 21:35:30 +0700

    Create button change step number using state and handling event

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = ({
+      val : 0
+    })
+
+    this.onClickUp = this.onClickUp.bind(this);
+    this.onClickDown = this.onClickDown.bind(this);
+  }
+
+  onClickDown(){
+    this.setState({
+      val: this.state.val - 1
+    })
+  }
+
+  onClickUp(){
+    this.setState({
+      val: this.state.val + 1
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <button onClick={this.onClickDown} id="subBtn">-</button>
+        <p id="numberPreview">{this.state.val}</p>
+        <button onClick={this.onClickUp} id="plusBtn">+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
